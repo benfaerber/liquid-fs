5bc4b2e7991929e921f26dfb8871869e36a2db16
Ben Faerber <benfaerber@protonmail.com>
2022-03-08 15:49:43 -0700

    Token Lexer

--- LiquidLexer.fs
+++ LiquidLexer.fs
@@ -33,6 +33,7 @@ type token =
   | Identifier of string
   | Boolean of bool
   | String of string
+  | Number of float
 
 
 let starts_with_regex txt = "^(" + txt + ")"
@@ -68,77 +69,34 @@ let token_to_string =
   | Identifier id -> sprintf "Identifier (%s)" id
   | Boolean b -> sprintf "Boolean (%s)" (if b then "True" else "False")
   | String s -> sprintf "String (%s)" s
+  | Number n -> sprintf "Number (%f)" n
 
-let token_to_keyword_string =
-  function
-  | Assign -> Some "assign"
-  | Increment -> Some "increment"
-  | Decrement -> Some "decrement"
-  | Render -> Some "render"
-  | Include -> Some "include"
-  | If -> Some "if"
-  | Else -> Some "else"
-  | EndIf -> Some "endif"
-  | For -> Some "for"
-  | EndFor -> Some "endfor"
-  | Case -> Some "case"
-  | EndCase -> Some "endcase"
-  | When -> Some "when"
-  | Unless -> Some "unless"
-  | EndUnless -> Some "endunless"
-  | Comment -> Some "comment"
-  | EndComment -> Some "endcomment"
-  | _ -> None
-
-let keyword_to_token =
-  function
-  | "assign" -> Some Assign
-  | "increment" -> Some Increment
-  | "decrement" -> Some Decrement
-  | "render" -> Some Render
-  | "include" -> Some Include
-  | "if" -> Some If
-  | "else" -> Some Else
-  | "endif" -> Some EndIf
-  | "case" -> Some Case
-  | "endcase" -> Some EndCase
-  | "when" -> Some When
-  | "unless" -> Some Unless
-  | "endunless" -> Some EndUnless
-  | "comment" -> Some Comment
-  | "endcomment" -> Some EndComment
-  | _ -> None
 
 let lex_keyword (s: string) =
   let keywords =
-    [ Assign
-      Increment
-      Decrement
-      Render
-      Include
-      If
-      Else
-      EndIf
-      For
-      EndFor
-      Case
-      EndCase
-      When
-      Unless
-      EndUnless
-      Comment
-      EndComment ] in
+    [ Assign, "assign"
+      Increment, "increment"
+      Decrement, "decrement"
+      Render, "render"
+      Include, "include"
+      If, "if"
+      Else, "else"
+      EndIf, "endif"
+      For, "for"
+      EndFor, "endfor"
+      Case, "case"
+      EndCase, "endcase"
+      When, "when"
+      Unless, "unless"
+      EndUnless, "endunless"
+      Comment, "comment"
+      EndComment, "endcomment" ] in
 
   let found_keyword =
-    (List.tryFind
-      (fun k ->
-        match token_to_keyword_string k with
-        | Some literal -> Regex.IsMatch(s, starts_with_regex literal)
-        | _ -> false)
-      keywords) in
+    (List.tryFind (fun (_, literal) -> Regex.IsMatch(s, starts_with_regex literal)) keywords) in
 
   match found_keyword with
-  | Some fk -> Some fk, s[(token_to_keyword_string fk).Value.Length ..]
+  | Some (token, literal) -> Some token, s[literal.Length ..]
   | None -> None, s
 
 
@@ -176,12 +134,49 @@ let lex_string (s: string) =
 
 let lex_operator (s: string) =
   let operators =
-    [ "=", Eq
-      "==", EqEq
+    [ "==", EqEq
+      "=", Eq
       "!=", Ne
-      ">", Gt
-      "<", Lt
       ">=", Gte
       "<=", Lte
+      ">", Gt
+      "<", Lt
       "or", Or
       "and", And ] in
+
+  let found =
+    List.tryFind (fun (literal, _) -> Regex.IsMatch(s, starts_with_regex literal)) operators in
+
+  match found with
+  | Some (l, t) -> Some t, s[l.Length ..]
+  | None -> None, s
+
+let lex_number (s: string) =
+  let number_regex = "((?:-)?(?:\d+)(?:\.)?(?:\d+)?)" in
+
+  if Regex.IsMatch(s, starts_with_regex number_regex) then
+    let m = Regex.Match(s, starts_with_regex number_regex) in
+    let literal = m.Groups.[0].Value in
+    Some(Number((float) literal)), s[literal.Length ..]
+  else
+    None, s
+
+let lex_token (s: string) =
+  let lexers =
+    [ lex_keyword
+      lex_number
+      lex_operator
+      lex_string
+      lex_bool ] in
+
+  let found_lexer =
+    List.tryFind
+      (fun lexer ->
+        (match lexer s with
+         | Some _, _ -> true
+         | _ -> false))
+      lexers in
+
+  match found_lexer with
+  | Some lexer -> lexer s
+  | None -> None, s
--- Program.fs
+++ Program.fs
@@ -5,10 +5,15 @@ let read_file filename = File.ReadAllText filename
 
 let tokens = BlockTokenizer.get_liquid_tokens (read_file "./test.liquid")
 // List.iter (fun block -> printfn "<---\n%s\n--->" block.Content) tokens
-let lexed, unlexed = LiquidLexer.lex_keyword "else apple = 12"
-let blexed, bunlexed = LiquidLexer.lex_bool "false apple = 12"
-let slexed, sunlexed = LiquidLexer.lex_string "\"This is a string\" apple = 12"
 
-match slexed with
-| Some x -> printfn "%s" (LiquidLexer.token_to_string x)
-| None -> printfn "None"
+let print_lex =
+  function
+  | Some t, rest -> printfn "Lexed: %s" (LiquidLexer.token_to_string t)
+  | None, rest -> printfn "Lex Failed!"
+
+print_lex (LiquidLexer.lex_keyword "else apple = 12")
+print_lex (LiquidLexer.lex_bool "false apple = 12")
+print_lex (LiquidLexer.lex_string "\"This is a string\" apple = 12")
+print_lex (LiquidLexer.lex_operator ">= 12")
+print_lex (LiquidLexer.lex_number "-100.84= 12")
+print_lex (LiquidLexer.lex_token "x")
