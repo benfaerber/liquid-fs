4c7cc5a33cd67703a11bfeedb39a74e653d72eb6
Ben Faerber <benfaerber@protonmail.com>
2022-03-08 10:22:27 -0700

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿open System.Text.RegularExpressions
+open System.IO
+
+printfn "Hello from F#"
+
+let block_regex = "\{\%(\-)?(.+)(-)?\%\}"
+let output_regex = "\{\{(?:\s+)?(.+?)(?:\s+)?\}\}"
+
+let read_file filename = File.ReadAllText filename
+
+printfn "%s" (read_file "./test.liquid")
